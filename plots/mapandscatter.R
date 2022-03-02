ed68b49bec3723eb8f49f6e97c7e39307a7b73a3
petedodd <p.j.dodd@sheffield.ac.uk>
2022-03-02 11:15:06 +0000

    propagating percent_change update

--- plots/mapandscatter.R
+++ plots/mapandscatter.R
@@ -29,7 +29,9 @@ CF <- merge(CF,mpd[age_group=='014',.(iso3,percent_change)],by='iso3')
 
 ggplot(CF,aes(cedpc,percent_change,label=iso3)) +
   geom_point() + geom_text_repel()+
-  xlab('Estimated cumulative COVID deaths per 100K prior to 2021')+
-  ylab('Percent change in TB notifications')
+  xlab('Estimated cumulative COVID deaths per 100K prior to 2021 (square root scale)')+
+  ylab('Percent change in TB notifications')+
+  scale_x_sqrt()+
+  geom_abline(yintercept=0,slope=0,col=2)
 
-ggsave(here('plots/COVIDvTB.pdf'),w=10,h=7)
+ggsave(here('plots/COVIDvTB.pdf'),w=15,h=10)
